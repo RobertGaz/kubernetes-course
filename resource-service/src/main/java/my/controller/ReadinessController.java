133a530a5424b7a74ae18b0014b6e65b0c4e107e
RobertGaz <gazizov.robert@gmail.com>
2023-12-17 21:14:39 +0400

    hw 4 added

--- resource-service/src/main/java/my/controller/ReadinessController.java
+++ resource-service/src/main/java/my/controller/ReadinessController.java
@@ -1,5 +1,8 @@
 package my.controller;
 
+import my.service.Mp3GetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,9 @@ import java.util.Random;
 
 @RestController
 public class ReadinessController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadinessController.class);
+
     @GetMapping("/readiness")
     public ResponseEntity<String> getReadiness() {
 
@@ -16,9 +22,11 @@ public class ReadinessController {
         Random rand = new Random();
         int randomValue = rand.nextInt(100);
         if (randomValue > 80) {
+            LOGGER.info("Service readiness is: NOT READY.");
             return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
 
+        LOGGER.info("Service readiness is: READY.");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- song-service/src/main/java/my/controller/ReadinessController.java
+++ song-service/src/main/java/my/controller/ReadinessController.java
@@ -1,5 +1,7 @@
 package my.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import java.util.Random;
 
 @RestController
 public class ReadinessController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadinessController.class);
+
     @GetMapping("/readiness")
     public ResponseEntity<String> getReadiness() {
 
@@ -16,9 +21,11 @@ public class ReadinessController {
         Random rand = new Random();
         int randomValue = rand.nextInt(100);
         if (randomValue > 80) {
+            LOGGER.info("Service readiness is: NOT READY.");
             return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
 
+        LOGGER.info("Service readiness is: READY.");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
